8d3747b8f91505127485cd6ef3ac1e0b4900b401
bregord <sirbrendang@gmail.com>
2014-05-04 23:05:12 -0400

    just an easy ruby and python challenge.

---
+++ CoderBytes/Easy/Python/firstreverse.py
@@ -0,0 +1,29 @@
+def FirstReverse(str): 
+
+    text =''
+    i = len(str)-1
+    while(i>=0):
+        text+=str[i]
+        i=i-1
+    
+
+    return text
+    
+print FirstReverse(raw_input())  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
