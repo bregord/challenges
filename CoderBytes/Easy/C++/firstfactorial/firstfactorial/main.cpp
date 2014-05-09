2a7d342a83fefea5c3ff9a3c32d3150d80ca4af5
bregord <sirbrendang@gmail.com>
2014-05-08 23:29:06 -0500

    I'd like to do more of these, but I really am very lazy.

---
+++ CoderBytes/Easy/C++/firstfactorial/firstfactorial/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  firstfactorial
+//
+//  Created by Brendan on 2014-05-08.
+//  Copyright (c) 2014 brendan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
